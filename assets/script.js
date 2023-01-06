dd8108af302d27ebafb1364f6d776be01048d356
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-05 22:00:13 -0600

    added final score and leaderboard to html

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,27 @@ const startBtn = document.getElementById('start-button')
 
 const questionContainer = document.getElementById('question-container')
 
+const questionElement = document.getElementById('question')
+const answerBtnElement = document.getElementById('answer-btn')
+
 const shuffleQuestions, currentQuestionIndex
 
-.startBtn.addEventListener('click', startQuiz)
+startBtn.addEventListener('click', startQuiz)
 
 function startQuiz () {
     console.log('started')
     startBtn.classList.add('hide')
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainer.classList.remove('hide')
     setNextQuestion()
 }
 
 function nextQuestion () {
-
+    showQuestion(shuffleQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question)
 
 function selectAnswer() {
 
@@ -23,10 +30,12 @@ function selectAnswer() {
 
 const questions = {
     {
-        question: ''
+        question: 'where is the correct place to insert a JavaScript?',
         answers: [
-            {text:'', correct: true},
-            {text:'', correct: false}
+            {text: 'in the <body> and <head> section', correct: true},
+            {text: 'in the <head> section only', correct: false},
+            {text: 'in the <body> section only', correct: false},
+            {text: 'in the stylesheet', correct: false}
         ]
     }
 }
