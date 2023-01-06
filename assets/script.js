bc31be4dfca6f1fc9534bebddb44654dc5036a5e
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-05 22:34:08 -0600

    defined resetState, all 3 status class for answers, and added event listeners for start and next buttons

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,66 @@
 const startBtn = document.getElementById('start-button')
 
+const nextBtn = document.getElementById('next-button')
+
 const questionContainer = document.getElementById('question-container')
 
 const questionElement = document.getElementById('question')
+
 const answerBtnElement = document.getElementById('answer-btn')
 
-const shuffleQuestions, currentQuestionIndex
+let shuffleQuestions, currentQuestionIndex
 
 startBtn.addEventListener('click', startQuiz)
+nextBtn.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startQuiz () {
-    console.log('started')
     startBtn.classList.add('hide')
     shuffleQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainer.classList.remove('hide')
-    setNextQuestion()
+    nextQuestion()
 }
 
 function nextQuestion () {
+    resetState()
     showQuestion(shuffleQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question)
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextBtn.classList.add('hide')
+    while (answerBtnElement.firstChild) {
+        answerBtnElement.removeChild(answerBtnElement.firstChild)
+    }
+}
 
 function selectAnswer() {
+    const
 
 }
 
-const questions = {
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
     {
         question: 'where is the correct place to insert a JavaScript?',
         answers: [
@@ -38,4 +70,4 @@ const questions = {
             {text: 'in the stylesheet', correct: false}
         ]
     }
-}
+]
