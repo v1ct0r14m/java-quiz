ea6e74894ad5ffd46fc8ba4429ec78dc69ba5bc8
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-05 21:32:43 -0600

    startQuiz function created, working on questions

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,16 @@
 const startBtn = document.getElementById('start-button')
 
-function startQuiz () {
+const questionContainer = document.getElementById('question-container')
 
+const shuffleQuestions, currentQuestionIndex
+
+.startBtn.addEventListener('click', startQuiz)
+
+function startQuiz () {
+    console.log('started')
+    startBtn.classList.add('hide')
+    questionContainer.classList.remove('hide')
+    setNextQuestion()
 }
 
 function nextQuestion () {
@@ -10,4 +19,14 @@ function nextQuestion () {
 
 function selectAnswer() {
 
+}
+
+const questions = {
+    {
+        question: ''
+        answers: [
+            {text:'', correct: true},
+            {text:'', correct: false}
+        ]
+    }
 }
